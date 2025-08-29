80284d660b41a9495ed0dda6ef26b92bd5069e57
AlternativeEnergyMaterials <134429878+AlternativeEnergyMaterials@users.noreply.github.com>
2025-08-29 11:39:38 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y git libgpiod-dev socat
-sudo nmcli con modify "Wired connection 1" ipv4.method manual ipv4.addresses 192.168.7.2/24
+# sudo apt update -y && sudo apt upgrade -y
+# sudo apt install -y git libgpiod-dev socat
+# sudo nmcli con modify "Wired connection 1" ipv4.method manual ipv4.addresses 192.168.7.2/24
 
-sudo apt install python3-pip 
-pip install pi-plates --break-system-packages
-pip install pyyaml --break-system-packages 
+# sudo apt install python3-pip 
+# pip install pi-plates --break-system-packages
+# pip install pyyaml --break-system-packages 
 pip install sm16relind --break-system-packages 
 pip install sm8mosind --break-system-packages 
 pip install smtc --break-system-packages 
@@ -38,10 +38,10 @@ sudo make install
 sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_spi 0
 
-cd ~/ 
-cp picode/{*.py,*.sh,*.yaml} ~/
+# cd ~/ 
+# cp picode/{*.py,*.sh,*.yaml} ~/
 
-sed -i "s/USER/$(whoami)/g" picode/wd_run.service
-sudo cp picode/wd_run.service /etc/systemd/system/wd_run.service
-sudo systemctl daemon-reload
-sudo systemctl enable wd_run.service
+# sed -i "s/USER/$(whoami)/g" picode/wd_run.service
+# sudo cp picode/wd_run.service /etc/systemd/system/wd_run.service
+# sudo systemctl daemon-reload
+# sudo systemctl enable wd_run.service
